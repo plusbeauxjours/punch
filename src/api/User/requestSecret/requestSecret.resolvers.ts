5bdf1fea0edd9d5209232f74e887c7205b6efdd0
plusbeauxjours <plusbeauxjours@gmail.com>
2019-07-04 16:42:19 +0900

    context

--- src/api/User/requestSecret/requestSecret.resolvers.ts
+++ src/api/User/requestSecret/requestSecret.resolvers.ts
@@ -4,11 +4,10 @@ import { prisma } from "../../../../generated/prisma-client/index";
 
 export default {
   Mutation: {
-    requestSecret: async (_, args, { request }) => {
+    requestSecret: async (_, args) => {
       const { email } = args;
       const loginSecret = generateSecret();
       try {
-        throw Error();
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,11 @@ import app from "./app";
 const PORT: number | string = process.env.PORT || 4000;
 const PLAYGROUND_ENDPOINT: string = "/playground";
 const GRAPHQL_ENDPOINT: string = "/graphql";
-const SUBSCRIPTION_ENDPOINT: string = "/subscription";
 
 const appOptions: Options = {
   port: PORT,
   playground: PLAYGROUND_ENDPOINT,
-  endpoint: GRAPHQL_ENDPOINT,
-  subscriptions: {
-    path: SUBSCRIPTION_ENDPOINT,
-    onConnect: async connectionParams => {
-      throw new Error("❌ No JWT. Can't subscribe");
-    }
-  }
+  endpoint: GRAPHQL_ENDPOINT
 };
 
 const handleAppStart = () =>
